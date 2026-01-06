d7ab3271bd7799ae012136f252197251a4e0be5c
Judah Fuller <judah@judahfuller.com>
2026-01-06 09:22:23 +0000

    Apply suggestions from code review

--- crates/settings/src/vscode_import.rs
+++ crates/settings/src/vscode_import.rs
@@ -764,8 +764,7 @@ impl VsCodeSettings {
         let platform = "osx";
         #[cfg(target_os = "freebsd")]
         let platform = "freebsd";
-        #[cfg(target_os = "ios")]
-        let platform = "ios";
+
         let env = self
             .read_value(&format!("terminal.integrated.env.{platform}"))
             .and_then(|v| v.as_object())
